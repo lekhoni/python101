7008ec9c6e8630ef81e32192192103d654bfb20c
Boltu Robotics <info@boltu.ai>
2024-01-01 18:11:33 -0600

    Added lesson1 instructions

---
+++ lesson1/README.md
@@ -0,0 +1,19 @@
+# Test variables knowledge
+
+## Problem set
+* 1: Define an integer variable named 'number' and assign the value 10 to it
+* 2: Define a string variable named 'name' and assign the name Karim to it
+* 3: Define a float variable named 'pi' with value of 3.1415926
+* 4: Define a boolean variable named 'is_valid' with False value
+* 5: Define a dictionary variable named 'user_info' and assign name to Karim and age to 20 to it
+* 6: Define a list variable named 'color_list' and assign these colors to it: red, green, blue
+
+Open the file named variables.py and complete all problems.
+
+You may test it using following command from this directory:
+
+```
+python -m unittest test_variables
+```
+
+Only submit the variables.py to the judge.
--- lesson1/test_variables.py
+++ lesson1/test_variables.py
@@ -1,33 +1,37 @@
 
 import unittest
 
-from variables import number, name, pi, is_valid, user_info, color_list
-
 
 class TestVariables(unittest.TestCase):
 
     def test_number(self):
         """Test for integer variable"""
+        from variables import number
         self.assertIsInstance(number, int)
 
     def test_name(self):
+        from variables import name
         """Test for string variable"""
         self.assertIsInstance(name, str)
 
     def test_pi(self):
         """Test for float variable"""
+        from variables import pi
         self.assertIsInstance(pi, float)
 
     def test_is_valid(self):
         """Test for boolean variable"""
+        from variables import is_valid
         self.assertIsInstance(is_valid, bool)
 
     def test_user_info(self):
         """Test for dictionary variable"""
+        from variables import user_info
         self.assertIsInstance(user_info, dict)
 
     def test_color_list(self):
         """Test for list variable"""
+        from variables import color_list
         self.assertIsInstance(color_list, list)
 
 
