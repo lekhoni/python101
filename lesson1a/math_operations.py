579b9bc854ebd3e0bca669f4ba3657f1d940b2d0
Boltu Robotics <info@boltu.ai>
2024-01-03 08:17:04 -0600

    Fixed solution file

--- lesson1a/math_operations.py
+++ lesson1a/math_operations.py
@@ -2,73 +2,49 @@ import math
 
 # TODO: Implement add - return sum of parameters a and b
 def add(a, b):
-    result = 0
-    #TODO: Implement here     
-    return result 
+    return a + b
 
 # TODO: Implement subtract - return difference of parameters a and b
 def subtract(a, b):
-    result = 0
-    #TODO: Implement here
-    return result 
+    return a - b
 
 # TODO: Implement multiply - return product of parameters a and b
 def multiply(a, b):
-    result = 0
-    #TODO: Implement here
-    return result 
+    return a * b
 
 # TODO: Implement divide - return quotient of parameters a and b
 def divide(a, b):
     # remember to handle division by zero in tests
-    result = 0
-    #TODO: Implement here
-    return result 
+    return a / b if b != 0 else 0
 
 
 # TODO: Implement square - return square of parameter n
 def square(n):
-    result = 0
-    #TODO: Implement here
-    return result 
+    return n**2
 
 
 # TODO: Implement cube - return cube of parameter n
 def cube(n):
-    result = 0
-    #TODO: Implement here
-    return result 
+    return n**3
 
 
 # TODO: Implement modulus - return remainder of division of a by b
 def modulus(a, b):
-    result = 0
-    #TODO: Implement here
-    return result 
+    return a % b
 
-
-# TODO: Implement square root function
+# TODO: Implement square root
 def sqrt(n):
-    result = 0
-    #TODO: Implement here
-    return result 
+    return math.sqrt(n)
 
 # TODO: Implement sine function using math library
 def sin(x):
-    result = 0
-    #TODO: Implement here
-    return result 
-
+    return math.sin(x)
 
 # TODO: Implement cosine function
 def cos(x):
-    result = 0
-    #TODO: Implement here
-    return result 
-
+    return math.cos(x)
 
 # TODO: Implement natural logarithm
 def log(x):
-    result = 0
-    #TODO: Implement here
-    return result 
+    return math.log(x)
+
