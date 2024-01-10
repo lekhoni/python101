3793a8517276a4c69d9fba51b4b3b5fede7436cd
Boltu Robotics <info@boltu.ai>
2024-01-10 07:48:29 -0600

    Added more functions in for loop operations

--- lesson4/for_loop_operations.py
+++ lesson4/for_loop_operations.py
@@ -1,3 +1,32 @@
+def print_numbers(end):
+    """
+    Prints all numbers from 1 to end (inclusive).
+    """
+    for i in range(end):
+        print(i + 1)
+
+def print_numbers_reverse(end):
+    """
+    Prints all numbers from end to 1 (inclusive).
+    """
+    for i in range(end):
+        print(end - i)
+
+def print_numbers_even(end):
+    """
+    Prints all even numbers from 1 to end (inclusive).
+    """
+    for i in range(end):
+        if (i + 1) % 2 == 0:
+            print(i + 1)
+
+def print_numbers_even_reverse(end):
+    """
+    Prints all even numbers from end to 1 (inclusive).
+    """
+    for i in range(end):
+        if (end - i) % 2 == 0:
+            print(end - i)
 
 def sum_numbers(n):
     """
@@ -17,7 +46,6 @@ def multiply_list(lst):
         result *= element
     return result
 
-# TODO: Implement count_occurrences
 def count_occurrences(string, character):
     """
     Returns the number of times a character occurs in a string.
@@ -28,7 +56,6 @@ def count_occurrences(string, character):
             count += 1
     return count
 
-# TODO: Implement accumulate_even_numbers
 def accumulate_even_numbers(limit):
     """
     Returns a list of even numbers up to a given limit (inclusive).
@@ -38,3 +65,37 @@ def accumulate_even_numbers(limit):
         if i % 2 == 0:
             evens.append(i)
     return evens
+
+def find_min(numbers):
+    """
+    Returns the smallest number in a list.
+    """
+    min_num = numbers[0]
+    for num in numbers:
+        if num < min_num:
+            min_num = num
+    return min_num
+
+def find_max(numbers):
+    """
+    Returns the largest number in a list.
+    """
+    max_num = numbers[0]
+    for num in numbers:
+        if num > max_num:
+            max_num = num
+    return max_num    
+
+def max_min_diff(numbers):
+    """
+    Returns the difference between the largest and smallest numbers in a list.
+    """
+    max_num = numbers[0]
+    min_num = numbers[0]
+    for num in numbers:
+        if num > max_num:
+            max_num = num
+        elif num < min_num:
+            min_num = num
+    return max_num - min_num
+
